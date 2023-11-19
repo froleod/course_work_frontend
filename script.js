8befddc3e32fa80a80fdd8d22188aa4e7ca29b40
Leonid Frolov <froleod@gmail.com>
2023-11-19 13:05:53 +0300

    added db form

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
-function onEntry(entry){
-    entry.forEach(change => {
-        if(change.isIntersecting){
-            change.target.classList.add('show');
-        } else{
-            change.target.classList.remove('show');
-        }
-    });
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const databases = ["PostgreSQL", "OracleBD", "MySQL", "SQLite"];
+    const checkboxes = document.querySelectorAll('.filterCheckbox');
+    const resultDiv = document.getElementById('result');
 
-let options = {threshold: [0.5] };
-let observer = new IntersectionObserver(onEntry, options);
-let elements = document.querySelectorAll('.appear');
-for (let elm of elements){
-    observer.observe(elm);
-}
+    function filterDatabases() {
+        const checkedFilters = Array.from(checkboxes).filter(checkbox => checkbox.checked).map(checkbox => checkbox.name);
+
+        let filteredDatabases = databases.slice(); // Создаем копию изначального массива
+
+        if (checkedFilters.includes('openSourceCheckbox')) {
+            filteredDatabases = filteredDatabases.filter(db => ['PostgreSQL', 'MySQL', 'SQLite'].includes(db));
+        }
+        if (checkedFilters.includes('easyToInstallCheckbox')) {
+            filteredDatabases = filteredDatabases.filter(db => ['PostgreSQL', 'MySQL', 'SQLite'].includes(db));
+        }
+        if (checkedFilters.includes('lightnessCheckbox')) {
+            filteredDatabases = filteredDatabases.filter(db => db === 'SQLite');
+        }
+        if (checkedFilters.includes('performanceCheckbox')) {
+            filteredDatabases = filteredDatabases.filter(db => ['PostgreSQL', 'OracleBD'].includes(db));
+        }
+        if (checkedFilters.includes('JSONCheckbox')) {
+            filteredDatabases = filteredDatabases.filter(db => db === 'PostgreSQL');
+        }
+
+        displayResults(filteredDatabases);
+    }
+
+    function displayResults(databasesToShow) {
+        resultDiv.innerHTML = '';
+        if (databasesToShow.length === 0) {
+            resultDiv.innerHTML = 'Баз данных по таким критериям нет';
+        } else {
+            const ul = document.createElement('ul');
+            databasesToShow.forEach(db => {
+                const li = document.createElement('li');
+                li.textContent = db;
+                ul.appendChild(li);
+            });
+            resultDiv.appendChild(ul);
+        }
+    }
+
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', filterDatabases);
+    });
+    filterDatabases();
+});
