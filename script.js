793703de0c7a0d4c62d1324812b7b444b1254bb8
Leonid Frolov <froleod@gmail.com>
2023-12-01 20:38:21 +0300

    upd footer last commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const databases = ["PostgreSQL", "OracleBD", "MySQL", "SQLite"];
     const checkboxes = document.querySelectorAll('.filterCheckbox');
     const resultDiv = document.getElementById('result');
